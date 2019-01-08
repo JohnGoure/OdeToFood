435ec037feb35c1701c841475bb8be58e3d37389
John Goure <johngoure@protonmail.com>
2019-01-07 21:11:00 -0600

    Intial Commit

---
+++ Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers {
+
+    [Route("[controller]")]
+    public class AboutController {
+        
+        [Route("")]
+        public string Phone() {
+            return "210-459-8845";
+        }
+
+        [Route("address")]
+        public string Address() {
+            return "San Antonio, Tx";
+        }
+    }
+}
---
+++ Controllers/HomeController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers {
+
+    public class HomeController {
+        public string Index() {
+            return "Hello from the home controller!";
+        }
+    }
+}

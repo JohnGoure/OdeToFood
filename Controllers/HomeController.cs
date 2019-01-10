23198653b3e232d953958be54606cbc3eb8eba9c
John Goure <johngoure@protonmail.com>
2019-01-09 20:45:50 -0600

    Implemented dependency injection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,19 @@
 using Microsoft.AspNetCore.Mvc;
 using OdeToFood.Models;
+using OdeToFood.Services;
 
 namespace OdeToFood.Controllers {
 
     public class HomeController : Controller {
+        private IRestaurantData _restaurantData;
+
+        public HomeController(IRestaurantData restaurantData)
+        {
+            this._restaurantData = restaurantData;
+        }
+        
         public IActionResult Index() {
-            var model = new Restraunt {
-                ID = 1,
-                Name = "Pizza Hut"
-            };
+            var model = this._restaurantData.GetAll();
             return View(model);
         }
     }
